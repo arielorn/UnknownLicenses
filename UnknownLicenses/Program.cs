47b1f296d772a17689b1e4d58139a89ce395a096
arielorn <ariel@snyk.io>
2019-02-20 13:22:02 +0200

    Initial commit

---
+++ UnknownLicenses/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace UnknownLicenses
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
